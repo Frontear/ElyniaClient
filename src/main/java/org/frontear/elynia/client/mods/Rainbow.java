02a1587e7c9e2498265d8f881caba697ed2b82a3
Ali Rizvi <frontear4594@gmail.com>
2018-11-19 21:15:00 -0500

    Turn rainbow into a hidden visual mod

--- src/main/java/org/frontear/elynia/client/mods/Rainbow.java
+++ src/main/java/org/frontear/elynia/client/mods/Rainbow.java
@@ -4,5 +4,5 @@ import org.frontear.elynia.client.mods.base.ModBase;
 import org.frontear.elynia.client.mods.base.ModInfo;
 import org.lwjgl.input.Keyboard;
 
-@ModInfo(name = "Rainbow", key = Keyboard.KEY_R)
+@ModInfo(name = "Rainbow", key = Keyboard.KEY_R, hide = true)
 public class Rainbow extends ModBase {}
